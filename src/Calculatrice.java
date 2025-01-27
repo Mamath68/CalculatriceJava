20a3f9e3deb3d0f4feb432006b7306aa631e7906
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-01-27 10:06:02 +0100

    calculatrice première version

---
+++ src/Calculatrice.java
@@ -0,0 +1,34 @@
+public class Calculatrice {
+
+    private final float num1;
+    private final float num2;
+    private final float num3;
+
+    public Calculatrice(float num1, float num2, float num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    public String addition() {
+        float operation = this.num1 + this.num2 * this.num3;
+        return this.num1 + " + " + this.num2 + " * " + this.num3 + " = " + operation;
+    }
+
+    public String soustraction() {
+        float operation = this.num1 - this.num2;
+        return this.num1 + " - " + this.num2 + " = " + operation;
+    }
+
+    public String multiplication() {
+        float operation = this.num1 * this.num2;
+        return this.num1 + " * " + this.num2 + " = " + operation;
+    }
+
+    public String division() {
+        float operation = this.num1 / this.num2;
+        return this.num1 + " / " + this.num2 + " = " + operation;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Calculatrice addition = new Calculatrice(10, 10, 5);
+        Calculatrice soustraction = new Calculatrice(77777, 66666, 5);
+        Calculatrice multiplication = new Calculatrice(12, 12, 5);
+        Calculatrice division = new Calculatrice(6, 5, 5);
+
+        System.out.println(addition.addition());
+        System.out.println(soustraction.soustraction());
+        System.out.println(multiplication.multiplication());
+        System.out.println(division.division());
     }
 }
